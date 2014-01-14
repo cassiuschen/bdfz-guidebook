b8d38c2d4dec31272ef2977ac47e481576f04339
Cassius Chen <chzsh1995@gmail.com>
2014-01-14 05:12:22 +0000

    finish build up front end

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @question = @article.questions.create(params[:question].permit(:title,:content))
+    @question = @article.questions.create(params[:question].permit(:title, :content))
     redirect_to article_path(@article)
   end
 end
