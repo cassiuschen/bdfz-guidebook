f88af867b5d148172043c7e56b92a768455f45fd
cassiusChen <chzsh1995@gmail.com>
2014-02-21 13:19:05 +0800

    finish article style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   $markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => false)
+
+  def login_with_cas
+  	if session[:cas_user] ==
+    	@current_user = User.find_by school_id: session[:cas_user]
+    else
+  		@current_user = User.create!(
+  				school_id: session[:cas_user].to_i,
+  				name: session[:cas_user],
+  				password: session[:cas_user]
+  			)
+    end
+  	#end
+  end
 end
