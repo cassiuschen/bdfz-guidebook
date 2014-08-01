91e9d54871312863c43160fa4feab56723ef01a5
cassiuschen <chzsh1995@gmail.com>
2014-08-01 14:57:39 +0800

    build a simple view for desktop

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,6 +1,25 @@
 class ExploreController < ApplicationController
-  def index
-    @raw_articles = Book.all.first.articles.all
+  #def index
+#    @raw_articles = Book.all.first.articles.all
+#    @articles = @raw_articles.sort_by {|a| a.order}
+#    time = Array.new
+#    for article in @articles
+#      time << article.updated_at
+#    end
+#    sort_time = time.sort!
+#    @last_edit = sort_time.last
+#    t = @last_edit.to_s
+#    p = /(....)-(..)-(..)/ 
+#    p.match(t)
+#    year = $1.to_i
+#    month = $2.to_i
+#    version = "#{year}年#{month}月修订版"
+#    @title = version
+#  end
+
+  def show
+    @book = Book.where("id = ?", explore_params.to_i).last
+    @raw_articles = @book.articles.all
     @articles = @raw_articles.sort_by {|a| a.order}
     time = Array.new
     for article in @articles
@@ -15,5 +34,10 @@ class ExploreController < ApplicationController
     month = $2.to_i
     version = "#{year}年#{month}月修订版"
     @title = version
+  end
+
+  private
+  def explore_params
+    params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get '/explore' => "explore#index"
+  #get '/explore' => "explore#index"
+  get '/explore/:id' => "explore#show"
   get "home/index"
   root 'home#index'
 
